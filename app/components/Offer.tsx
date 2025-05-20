696436f3ebf0ce74adae1cc7ec402c59f3156edc
Damilola Ibrahim <oloyedammy@gmail.com>
2025-05-20 05:43:35 -0700

    again434ee ttete

--- app/components/Offer.tsx
+++ app/components/Offer.tsx
@@ -10,7 +10,7 @@ function Offer() {
                     offerList.map((item) => (
                         <div key={item.id} className="basis-1/4 bg-white border-4 rounded-br-2xl rounded-tl-2xl">
                             <div className="max-h-[250px] overflow-hidden">
-                                <Image className='rounded-tl-2xl' src={item.image} height={300} width={300} alt='' />
+                                <Image className='rounded-tl-2xl min-h-1' src={item.image} height={300} width={300} alt='' />
                             </div>
                             <div className="p-4">
                                 <h2 className="font-bold text-center text-lg text-primary">
--- constants/constant.ts
+++ constants/constant.ts
@@ -36,14 +36,14 @@ export const supportList: SupportItem[] = [
 export const offerList: SupportItem[] = [
   {
     id: 0,
-    image: "/personalize.png",
-    title: "Personalized Support",
-    para: 'Personalized one-on-one guidance and tailored lessons designed to help you succeed.',
+    image: "/reading.jpg",
+    title: "Quran Recitation & Memorization",
+    para: 'Allah has promised to preserve the Quran—and He enables His servants to memorize it. Start your Hifz journey today from home with our proven method that makes memorization easy and recitation flawless.',
   },
   {
     id: 1,
-    image: "/progress.png",
-    title: "Progress Tracking",
+    image: "/praying.jpg",
+    title: "Tawheed & Fiqh",
     para: 'Track your progress by regularly evaluating your performance and receiving feedback.',
   },
   {
